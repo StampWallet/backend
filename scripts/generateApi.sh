bbfc614b61862dfe5bfe762aabc0bf4ac3f545e4
Franciszek Stachura <fbstachura@gmail.com>
2023-04-19 17:59:40 +0200

    Add OpenAPI specs, API generator scripts, generated models

---
+++ scripts/generateApi.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# https://openapi-generator.tech/docs/installation
+# download from https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.3.0/openapi-generator-cli-6.3.0.jar
+
+SCRIPTPATH=`realpath "$0"`
+SCRIPTDIR=`dirname $SCRIPTPATH`
+
+java -jar openapi-generator-cli-6.3.0.jar generate -g go-gin-server -i $SCRIPTDIR/../api/schema.yaml -c $SCRIPTDIR/../api/config.yaml --global-property models -o $SCRIPTDIR/../
