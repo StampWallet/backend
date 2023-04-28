4a378d4182f2f37d7c1d856565c859387ac76617
kidp330 <j.migdal42@gmail.com>
2023-04-28 05:09:37 +0200

    Add TestContextBuilder and ExtractResponse funcs

--- internal/testutils/utils.go
+++ internal/testutils/utils.go
@@ -1,9 +1,18 @@
 package testutils
 
 import (
+	"bytes"
 	"database/sql/driver"
+	"encoding/json"
+	"io"
+	"net/http/httptest"
+	"net/url"
+	"path"
 	"reflect"
+	"testing"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func MatchEntities(matcher interface{}, Obj interface{}) bool {
@@ -71,4 +80,54 @@ func (Anything) Match(v driver.Value) bool {
 
 func ReturnArg(arg interface{}) interface{} {
 	return arg
+}
+
+type TestContextBuilder struct{ Context *gin.Context }
+
+func NewTestContextBuilder(w *httptest.ResponseRecorder) *TestContextBuilder {
+	ctx, _ := gin.CreateTestContext(w)
+	return &TestContextBuilder{Context: ctx}
+}
+
+func (tc *TestContextBuilder) SetDefaultUser() *TestContextBuilder {
+	tc.Context.Set("user", GetDefaultUser())
+	return tc
+}
+
+func (tc *TestContextBuilder) SetUrl(argUrl *url.URL) *TestContextBuilder {
+	tc.Context.Request.URL = argUrl
+	return tc
+}
+
+func (tc *TestContextBuilder) SetDefaultUrl() *TestContextBuilder {
+	url, _ := url.Parse("localhost")
+	return tc.SetUrl(url)
+}
+
+func (tc *TestContextBuilder) SetEndpoint(endpointPath string) *TestContextBuilder {
+	rUrl := tc.Context.Request.URL
+	rUrl.Path = path.Join(rUrl.Path, endpointPath)
+	return tc
+}
+
+func (tc *TestContextBuilder) SetMethod(method string) *TestContextBuilder {
+	tc.Context.Request.Method = method
+	return tc
+}
+
+func (tc *TestContextBuilder) SetHeader(headerKey string, headerValue string) *TestContextBuilder {
+	tc.Context.Request.Header.Set(headerKey, headerValue)
+	return tc
+}
+
+func (tc *TestContextBuilder) SetBody(jsonBytes []byte) *TestContextBuilder {
+	tc.Context.Request.Body = io.NopCloser(bytes.NewBuffer(jsonBytes))
+	return tc
+}
+
+func ExtractResponse[T any](t *testing.T, w *httptest.ResponseRecorder) (*T, int, error) {
+	bodyBytes := w.Body.Bytes()
+	bodyPtr := new(T)
+	err := json.Unmarshal(bodyBytes, bodyPtr)
+	return bodyPtr, w.Code, err
 }
