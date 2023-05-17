dd9edd39dba2deb2abf3ef7f6d833c79f2e1ef4b
Franciszek Stachura <fbstachura@gmail.com>
2023-05-17 15:26:43 +0200

    Fix typo change error names

--- internal/database/accessors/accessors.go
+++ internal/database/accessors/accessors.go
@@ -9,11 +9,11 @@ import (
 	"gorm.io/gorm"
 )
 
-var NotFound = errors.New("Entity not found")
-var NoAccess = errors.New("No access to entity")
-var DatabaseError = errors.New("Database error")
+var ErrNotFound = errors.New("Entity not found")
+var ErrNoAccess = errors.New("No access to entity")
+var ErrDatabaseError = errors.New("Database error")
 
-// rule of thumb with managers and accessors: if a manager method requires an object, this probably means, that the object has to retrived with an accessors. and the accessor will check the (simple, in case of this app) permissions
+// rule of thumb with managers and accessors: if a manager method requires an object, this probably means, that the object has to be retrived with an accessor. and the accessor will check the (simple, in case of this app) permissions
 // if a manager requires only object id, that means that the action can be done by anyone
 // handlers should not access the database directly. thats what managers and accessors are for
 
