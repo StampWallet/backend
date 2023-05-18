ee19570086f3a845e0f7ba8727375bd9a84a7055
Franciszek Stachura <fbstachura@gmail.com>
2023-05-18 16:11:39 +0200

    Implement TokenServiceImpl

--- internal/services/token.go
+++ internal/services/token.go
@@ -2,16 +2,22 @@ package services
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
+	"github.com/lithammer/shortuuid/v4"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+
 	. "github.com/StampWallet/backend/internal/database"
+	. "github.com/StampWallet/backend/internal/utils"
 )
 
-var UnknownToken = errors.New("Invalid token")
+var ErrUnknownToken = errors.New("Invalid token")
 
 type TokenService interface {
 	Create(user *User, purpose TokenPurposeEnum, expiration time.Time) (*Token, string, error)
-	Check(tokenId string, tokenSecret string) (*User, *Token, error)
+	Check(tokenId string, tokenSecret string) (*Token, error)
 	Invalidate(token *Token) (*User, *Token, error)
 }
 
@@ -19,14 +25,68 @@ type TokenServiceImpl struct {
 	baseServices BaseServices
 }
 
+// TODO maybe TokenService should decide about expiration date instead of the caller
+// especially since it will handle
 func (service *TokenServiceImpl) Create(user *User, purpose TokenPurposeEnum, expiration time.Time) (*Token, string, error) {
-	return nil, "", nil
+	secret := shortuuid.New()
+	hash, err := bcrypt.GenerateFromPassword([]byte(secret), 10)
+	if err != nil {
+		return nil, "", fmt.Errorf("%s bcrypt failed to generate password: %+v", CallerFilename(), err)
+	}
+	token := Token{
+		TokenId:      shortuuid.New(),
+		TokenHash:    string(hash),
+		TokenPurpose: TokenPurposeSession,
+		Expires:      expiration,
+		Used:         false,
+		Recalled:     false,
+		User:         user,
+	}
+
+	tx := service.baseServices.Database.Create(&token)
+	if err := tx.GetError(); err != nil {
+		return nil, "", fmt.Errorf("%s database failed to create token: %+v", CallerFilename(), err)
+	}
+
+	return &token, secret, nil
 }
 
-func (service *TokenServiceImpl) Check(tokenId string, tokenSecret string) (*User, *Token, error) {
-	return nil, nil, nil
+func (service *TokenServiceImpl) Check(tokenId string, tokenSecret string) (*Token, error) {
+	var token *Token
+	tx := service.baseServices.Database.
+		Preload("User").
+		Find(token, Token{TokenId: tokenId, Recalled: false})
+	err := tx.GetError()
+	if err == gorm.ErrRecordNotFound {
+		return nil, ErrUnknownToken
+	} else if err != nil {
+		return nil, fmt.Errorf("%s database failed to find token: %+v", CallerFilename(), err)
+	}
+
+	if time.Now().After(token.Expires) {
+		return nil, ErrUnknownToken
+	}
+
+	token.Used = true
+	if token.TokenPurpose == TokenPurposeEmail && token.Used {
+		return nil, ErrUnknownToken
+	} else if token.TokenPurpose == TokenPurposeSession {
+		token.Expires = time.Now().Add(7 * 24 * time.Hour)
+	}
+
+	tx = service.baseServices.Database.Save(token)
+	if err := tx.GetError(); err != nil {
+		return nil, fmt.Errorf("%s database failed to update token: %+v", CallerFilename(), err)
+	}
+
+	return token, nil
 }
 
-func (service *TokenServiceImpl) Invalidate(token *Token) (*User, *Token, error) {
-	return nil, nil, nil
+func (service *TokenServiceImpl) Invalidate(token *Token) (*Token, error) {
+	token.Recalled = true
+	tx := service.baseServices.Database.Save(token)
+	if err := tx.GetError(); err != nil {
+		return nil, fmt.Errorf("%s database failed to update token: %+v", CallerFilename(), err)
+	}
+	return token, nil
 }
--- internal/services/token_test.go
+++ internal/services/token_test.go
@@ -59,14 +59,14 @@ func TestTokenServiceCheckValid(t *testing.T) {
 	user := GetTestUser(service.baseServices.Database)
 	token, secret := GetTestToken(service.baseServices.Database, user)
 
-	nUser, nToken, err := service.Check(token.TokenId, secret)
+	nToken, err := service.Check(token.TokenId, secret)
 	require.Nilf(t, err, "TokenService.Check should not return an error")
-	require.NotNilf(t, nUser, "User should not be nil")
+	require.NotNilf(t, nToken.User, "User should not be nil")
 	require.NotNilf(t, nToken, "Token should not be nil")
 	require.Equalf(t, token.TokenId, nToken.TokenId, "TokenService.Check should return the expected token")
 	require.Equalf(t, true, nToken.Used, "Token should be marked as used")
 
-	require.Equalf(t, nUser.PublicId, user.PublicId, "TokenService.Check should return the expected user")
+	require.Equalf(t, nToken.User.PublicId, user.PublicId, "TokenService.Check should return the expected user")
 }
 
 func TestTokenServiceCheckInvalidId(t *testing.T) {
@@ -75,9 +75,9 @@ func TestTokenServiceCheckInvalidId(t *testing.T) {
 	user := GetTestUser(service.baseServices.Database)
 	_, secret := GetTestToken(service.baseServices.Database, user)
 
-	nUser, nToken, err := service.Check("invalid id", secret)
-	require.ErrorAsf(t, err, UnknownToken, "TokenService.Check should return a UnknownToken error")
-	require.Nilf(t, nUser, "TokenService.Check should return a nil user")
+	nToken, err := service.Check("invalid id", secret)
+	require.ErrorAsf(t, err, ErrUnknownToken, "TokenService.Check should return a UnknownToken error")
+	require.Nilf(t, nToken.User, "TokenService.Check should return a nil user")
 	require.Nilf(t, nToken, "TokenService.Check should return a nil token")
 }
 
@@ -87,9 +87,9 @@ func TestTokenServiceCheckInvalidSecret(t *testing.T) {
 	user := GetTestUser(service.baseServices.Database)
 	token, secret := GetTestToken(service.baseServices.Database, user)
 
-	nUser, nToken, err := service.Check(token.TokenId, secret+"invalid")
-	require.ErrorAsf(t, err, UnknownToken, "TokenService.Check should return a UnknownToken error")
-	require.Nilf(t, nUser, "TokenService.Check should return a nil user")
+	nToken, err := service.Check(token.TokenId, secret+"invalid")
+	require.ErrorAsf(t, err, ErrUnknownToken, "TokenService.Check should return a UnknownToken error")
+	require.Nilf(t, nToken.User, "TokenService.Check should return a nil user")
 	require.Nilf(t, nToken, "TokenService.Check should return a nil token")
 }
 
@@ -99,17 +99,17 @@ func TestTokenServiceInvalidate(t *testing.T) {
 	user := GetTestUser(service.baseServices.Database)
 	token, secret := GetTestToken(service.baseServices.Database, user)
 
-	nUser, nToken, err := service.Invalidate(token)
+	nToken, err := service.Invalidate(token)
 	require.Nilf(t, err, "TokenService.Invalidate should return nil error")
-	require.NotNilf(t, nUser, "User should not be nil")
+	require.NotNilf(t, nToken.User, "User should not be nil")
 	require.NotNilf(t, nToken, "Token should not be nil")
 	require.Equalf(t, token.TokenId, nToken.TokenId, "TokenService.Invalidate should return the expected token")
 	require.Equalf(t, true, nToken.Recalled, "TokenService.Invalidate should return a recalled token")
-	require.Equalf(t, user.PublicId, nUser.PublicId, "TokenService.Invalidate should return the expected user")
+	require.Equalf(t, user.PublicId, nToken.User.PublicId, "TokenService.Invalidate should return the expected user")
 
-	testUser, testToken, err := service.Check(token.TokenId, secret)
-	require.ErrorAsf(t, err, UnknownToken, "TokenService.Check should return an error")
-	require.Nilf(t, testUser, "TokenService.Check should return a nil user")
+	testToken, err := service.Check(token.TokenId, secret)
+	require.ErrorAsf(t, err, ErrUnknownToken, "TokenService.Check should return an error")
+	require.Nilf(t, testToken.User, "TokenService.Check should return a nil user")
 	require.Nilf(t, testToken, "TokenService.Check should return a nil token")
 
 	var dbToken Token
